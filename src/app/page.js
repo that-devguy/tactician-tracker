eb40dd8afb49243f6a67795fe36ed6eaffcc7b37
that devguy <zkmutch@gmail.com>
2023-07-18 17:19:06 -0400

    installed axios, set up environment variables, stubbed player search

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,21 @@
+"use client"
+import { useState } from "react";
+import axios from "axios";
+
 export default function Home() {
+  const [searchText, setSearchText] = useState("");
+  function searchForPlayer(event) {
+    console.log()
+  }
+
   return (
     <main>
       <div>Home</div>
+      <div className="search-container">
+        <h4>Summoner Search</h4>
+        <input type="text" onChange={e => setSearchText(e.target.value)}></input>
+        <button onClick={e => searchForPlayer(e)}>Search</button>
+      </div>
     </main>
   );
 }
