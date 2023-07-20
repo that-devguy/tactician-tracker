e1cce125cce0eeef111f1a7fa9554e31307dd70b
that devguy <zkmutch@gmail.com>
2023-07-20 18:30:25 -0400

    added functions to extract players level and placement inside the MatchTile component by summonerId

--- src/app/components/MatchTile.js
+++ src/app/components/MatchTile.js
@@ -1,4 +1,39 @@
-export default async function matchTile(matchDetails) {
-  console.log(matchDetails);
-  return <div>{JSON.stringify(matchDetails)}</div>;
-}
+const MatchTile = ({ matchDetails, summonerId }) => {
+  // Extracting necessary information from matchDetails
+  const {
+    info: { participants },
+  } = matchDetails;
+
+  // Extracting participant levels
+  const getParticipantLevel = (summonerId) => {
+    const participant = participants.find(
+      (participant) => participant.puuid === summonerId
+    );
+    return participant ? participant.level : null;
+  };
+
+  // Extracting participant placements
+  const getParticipantPlacement = (summonerId) => {
+    const participant = participants.find(
+      (participant) => participant.puuid === summonerId
+    );
+    return participant ? participant.placement : null;
+  };
+
+  const level = getParticipantLevel(summonerId);
+  const placement = getParticipantPlacement(summonerId);
+
+  return (
+    <div className="mb-5">
+      <p>
+        Level: {level !== null ? level : "Summoner not found in match details"}
+      </p>
+      <p>
+        Placement:{" "}
+        {placement !== null ? placement : "Summoner not found in match details"}
+      </p>
+    </div>
+  );
+};
+
+export default MatchTile;
