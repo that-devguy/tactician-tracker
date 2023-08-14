ec1b3dd3dd2e800135598bcddd6a007dc4ce85d5
that devguy <zkmutch@gmail.com>
2023-08-14 16:44:22 -0400

    added soraka emote and updated the error page for summoner search

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,12 @@ import SummonerSearch from "@/app/components/SummonerSearch";
 export default function Home() {
   return (
     <section>
-      <div className="flex flex-col text-center items-center mt-14">
+      <div className="mt-14 flex flex-col items-center gap-2 text-center">
         <h1 className="text-4xl font-bold">Team Fight Tactics Stats</h1>
         <h2 className="text-lg font-semibold">
           Find Detailed Team Fight Tactics Statistics and Leaderboards
         </h2>
-        <div className="flex justify-center mx-auto w-1/2 py-2">
+        <div className="mx-auto flex w-1/2 justify-center">
           <SummonerSearch />
         </div>
       </div>
--- src/app/profile/[summonerName]/error.js
+++ src/app/profile/[summonerName]/error.js
@@ -1,6 +1,7 @@
 "use client"; // Error components must be Client Components
 
 import { useEffect } from "react";
+import Image from "next/image";
 
 export default function Error({ error, reset }) {
   useEffect(() => {
@@ -9,16 +10,34 @@ export default function Error({ error, reset }) {
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="flex flex-col justify-center mt-10">
+      <div className="flex items-center justify-center text-9xl font-black">
+        <p className="relative left-16">O</p>
+        <Image
+          className=""
+          src="/Everything_is_Okay_Emote.png"
+          alt="Everything is Okay Emote"
+          height={256}
+          width={256}
+          layout="fixed"
+        />
+        <p className="relative right-14">PS</p>
+      </div>
+      <div className="flex flex-col justify-center gap-3">
+        <h2 className="text-center text-xl font-bold">Something went wrong!</h2>
+        <p className="mx-auto max-w-md text-center text-sm">
+          Make sure the summoner you&apos;re trying to search exists and
+          that there are no spelling mistakes.
+        </p>
+        <button
+          onClick={
+            // Attempt to recover by trying to re-render the segment
+            () => reset()
+          }
+        >
+          Try again
+        </button>
+      </div>
     </div>
   );
 }
