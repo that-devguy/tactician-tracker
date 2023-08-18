ca3c238cb7e0dcc9f5b5aa46c1d40cd7bb669694
that_devguy <zkmutch@gmail.com>
2023-08-18 18:26:30 -0400

    added lib function that pulls the challenger league data and imported it into the leaderboards page

--- src/app/leaderboards/page.js
+++ src/app/leaderboards/page.js
@@ -1,4 +1,10 @@
-export default function Leaderboards() {
+import getChallengerLeague from "@/app/libs/getChallengerLeague";
+
+export default async function Leaderboards() {
+  const challengerLeague = await getChallengerLeague();
+  console.log(challengerLeague);
+  const leaderboards = challengerLeague.entries
+  console.log(leaderboards);
   return (
     <section className="px-10">
       <div>Leaderboards</div>
---
+++ src/app/libs/getChallengerLeague.js
@@ -0,0 +1,12 @@
+export default async function getChallengerLeague() {
+  const riotAPI = process.env.API_KEY;
+  const response = await fetch(
+    `https://na1.api.riotgames.com/tft/league/v1/challenger?api_key=${riotAPI}`,
+    { next: { revalidate: 3600 } }
+  );
+
+  if (!response.ok) {
+    throw new Error("failed to find leaderboard data");
+  }
+  return response.json();
+}
