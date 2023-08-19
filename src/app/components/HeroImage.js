4c76c17e5cd949f6f90b1e11c67f2ca512921ee9
that devguy <zkmutch@gmail.com>
2023-08-19 16:28:09 -0400

    built a static hero banner to house the header and search bar on the home page

--- src/app/components/HeroImage.js
+++ src/app/components/HeroImage.js
@@ -1,31 +1,36 @@
 import Image from "next/image";
 
-export default function HeroImage() {
+export default function heroImage() {
   return (
-    <div className="max-w-5xl">
-      <div className="relative">
-        <Image
-          src="/layer-1.png"
-          height={1298}
-          width={2560}
-          alt="layer-1"
-          className="absolute top-0 left-0"
-        />
-        <Image
-          src="/layer-2.png"
-          height={1298}
-          width={2560}
-          alt="layer-2"
-          className="absolute top-0 left-0"
-        />
-        <Image
-          src="/layer-3.png"
-          height={1298}
-          width={2560}
-          alt="layer-3"
-          className="absolute top-0 left-0"
-        />
-      </div>
+    <div className="relative max-h-[28rem] select-none bg-gradient-to-t from-brand-main to-[#F8A634]">
+      <Image
+        src="/layer-3.png"
+        height="1298"
+        width="2560"
+        alt="layer-3"
+        className="absolute top-10 z-30"
+      />
+      <Image
+        src="/layer-2.png"
+        height="1298"
+        width="2560"
+        alt="layer-2"
+        className="absolute top-10 z-20"
+      />
+      <Image
+        src="/layer-1.png"
+        height="1298"
+        width="2560"
+        alt="layer-1"
+        className="absolute top-10 z-10"
+      />
+      <Image
+        src="/layer-background.png"
+        height="1298"
+        width="2560"
+        alt="layer-1"
+        className=""
+      />
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,19 @@
-import Image from "next/image";
 import SummonerSearch from "@/app/components/SummonerSearch";
 import HeroImage from "@/app/components/HeroImage";
 
 export default function Home() {
   return (
-    <section>
-      <div className="mt-14 flex flex-col items-center gap-2 px-10 text-center">
-        <h1 className="text-6xl font-bold">Teamfight Tactics Stats</h1>
-        <h2 className="text-lg">
+    <section className="px-10">
+      <div className="mt-24 flex flex-col items-center gap-2 text-center">
+        <h1 className="z-50 text-6xl font-bold">Teamfight Tactics Stats</h1>
+        <h2 className="z-50 text-lg">
           Find Detailed Teamfight Tactics Statistics and Leaderboards
         </h2>
-        <div className="mx-auto flex w-1/2 justify-center">
+        <div className="z-50 mx-auto flex w-1/2 justify-center">
           <SummonerSearch />
         </div>
+      </div>
+      <div className="h-92 mx-auto -mt-48 max-w-6xl overflow-hidden rounded-lg">
         <HeroImage />
       </div>
     </section>
