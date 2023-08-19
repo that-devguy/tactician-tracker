bc704a1d271de67992f17c527a140882d93984d8
that_devguy <zkmutch@gmail.com>
2023-08-18 23:44:45 -0400

    moved the leaderboard into it's own component to import into the leaderboard board

--- src/app/leaderboards/page.js
+++ src/app/leaderboards/page.js
@@ -1,19 +1,10 @@
-import getLeaderboardData from "@/app/libs/getLeaderboardData";
+import Leaderboard from "@/app/components/Leaderboard";
 
 export default async function Leaderboards() {
-  const leaderboardData = await getLeaderboardData();
-  const leaderboards = leaderboardData.entries;
-
-  // Sort leaderboard results in descending order by LP
-  leaderboards.sort((a, b) => b.leaguePoints - a.leaguePoints);
-
-  console.log(leaderboards);
   return (
     <section className="px-10">
       <div>Leaderboards</div>
-      {leaderboards.slice(0, 20).map((leaderboard) => (
-        <p key={leaderboard.summonerId}>{leaderboard.summonerName}</p>
-      ))}
+      <Leaderboard />
     </section>
   );
 }
