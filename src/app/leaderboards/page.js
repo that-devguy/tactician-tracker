a77337734592bb421fc9b6253ea1611823c7128d
that devguy <zkmutch@gmail.com>
2023-08-18 20:34:05 -0400

    successfully return top 20 NA challenger players

--- src/app/leaderboards/page.js
+++ src/app/leaderboards/page.js
@@ -2,12 +2,14 @@ import getChallengerLeague from "@/app/libs/getChallengerLeague";
 
 export default async function Leaderboards() {
   const challengerLeague = await getChallengerLeague();
-  console.log(challengerLeague);
-  const leaderboards = challengerLeague.entries
+  const leaderboards = challengerLeague.entries;
   console.log(leaderboards);
   return (
     <section className="px-10">
       <div>Leaderboards</div>
+      {leaderboards.slice(0, 20).map((leaderboard) => (
+        <p key={leaderboard.summonerId}>{leaderboard.summonerName}</p>
+      ))}
     </section>
   );
 }
