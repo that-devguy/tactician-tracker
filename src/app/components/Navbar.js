82f900121a907d539a2c296f7430ac35e6367920
that devguy <zkmutch@gmail.com>
2023-08-23 11:23:10 -0400

    working on homepage and navbar responsiveness

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -90,7 +90,7 @@ export default function Navbar() {
   return (
     <nav
       ref={navbarRef}
-      className="fixed top-0 z-50 flex w-full flex-row items-center justify-between border-b-[0.5px] border-brand-bg2 bg-brand-bg px-4 py-5 md:px-10"
+      className="fixed top-0 z-50 flex h-16 w-full flex-row items-center justify-between border-b-[0.5px] border-brand-bg2 bg-brand-bg px-4 py-5 md:px-10"
     >
       <div className="navLogo-container flex items-center gap-10">
         <Link
@@ -98,7 +98,7 @@ export default function Navbar() {
           className="flex gap-2 px-2 text-sm font-bold md:px-2 md:text-lg"
         >
           Tactician Tracker
-          <span className="flex h-4 items-center justify-center rounded-[.25rem] bg-brand-secondary px-2 text-[.6rem] text-xs font-black text-brand-bg md:h-5 md:rounded-md">
+          <span className="flex h-4 items-center justify-center rounded-[.25rem] bg-brand-secondary px-2 text-[.6rem] font-black text-brand-bg md:h-5 md:rounded-md md:text-xs">
             BETA
           </span>
         </Link>
--- src/app/page.js
+++ src/app/page.js
@@ -6,18 +6,21 @@ import Image from "next/image";
 export default function Home() {
   return (
     <section className="md:px-10">
-      <div className="mt-24 flex flex-col items-center gap-2 text-center">
-        <h1 className="z-30 text-3xl md:text-6xl font-black">TACTICIAN TRACKER</h1>
+      <div className="relative flex h-fit flex-col items-center gap-2 text-center md:mt-24">
+        <h1 className="z-30 text-3xl font-black md:text-6xl">
+          TACTICIAN TRACKER
+        </h1>
         <h2 className="z-30 text-sm md:text-lg">
           Find Detailed Teamfight Tactics Stats, Leaderboards, and More.
         </h2>
         <div className="z-40 mx-auto flex w-1/2 justify-center">
           <SummonerSearch />
         </div>
+        <div className="absolute mx-auto mb-10 max-w-6xl overflow-hidden md:rounded-lg">
+          <HeroImage />
+        </div>
       </div>
-      <div className=" mx-auto -mt-44 md:-mt-48 mb-10 max-w-6xl overflow-hidden md:rounded-lg">
-        <HeroImage />
-      </div>
+
       <div className="mx-auto mb-5 max-w-6xl">
         <div className="mb-5 flex max-h-28 justify-between rounded-lg bg-gradient-to-tr from-[#ff571d] to-[#F8A634]">
           <div className="p-4">
