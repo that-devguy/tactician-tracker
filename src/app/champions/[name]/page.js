b0ce7b2191e9330d2a2b6801d8f04aea48ab1597
that_devguy <zkmutch@gmail.com>
2023-09-07 21:39:16 -0400

    added the route and page for champions by name

---
+++ src/app/champions/[name]/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Champion({ params: { name } }) {
+  return (
+    <section className="px-2 pt-8 md:px-6 lg:px-6">
+      <p>Ahri</p>
+    </section>
+  );
+}
