79f64c3f5238c6db71a47769d40c3f1f5bc7ac9f
that_devguy <zkmutch@gmail.com>
2023-09-08 00:08:53 -0400

    added the svg icons to the ChampionStats component

--- src/app/components/ChampionStats.js
+++ src/app/components/ChampionStats.js
@@ -6,6 +6,12 @@ export default function ChampionStats() {
       <div className="flex items-center gap-3 rounded-md px-4 md:px-0">
         Stats:
         <Image src="/health.svg" alt="Health Icon" height="14" width="14" />
+        <Image src="/damage.svg" alt="Damage Icon" height="14" width="14" />
+        <Image src="/dps.svg" alt="DPS Icon" height="14" width="14" />
+        <Image src="/attack_speed.svg" alt="Atk Spd Icon" height="14" width="14" />
+        <Image src="/armor.svg" alt="Armor Icon" height="14" width="14" />
+        <Image src="/magic_resist.svg" alt="MR Icon" height="14" width="14" />
+        <Image src="/mana.svg" alt="Mana Icon" height="14" width="14" />
       </div>
     </div>
   );
