f3f9a7dd199837a57e7e0c8c10fb33081069fc70
that devguy <zkmutch@gmail.com>
2023-09-12 11:51:02 -0400

    working update on ability desc variables

--- src/app/components/ChampionAbility.js
+++ src/app/components/ChampionAbility.js
@@ -1,6 +1,22 @@
 import Image from "next/image";
 
-export default function ChampionAbility() {
+export default function ChampionAbility({ selectedChampion }) {
+  const abilityIcon = selectedChampion.ability.icon.replace(".dds", ".png");
+
+  const fillVariables = (desc, variables) => {
+    return desc.replace(/@([^@]+)@/g, (match, varName) => {
+      const variableObject = variables.find(v => v.name === varName);
+      return variableObject ? variableObject.value[0] : match; // Using the first value in the value array
+    });
+  };
+
+  console.log(selectedChampion.ability.variables)
+
+  const cleanDesc = fillVariables(
+    selectedChampion.ability.desc,
+    selectedChampion.ability.variables
+  );
+
   return (
     <div className="mx-auto mb-2 flex max-w-lg select-none flex-col rounded-md bg-brand-bg2 p-4 md:max-w-none md:p-4">
       <div className="flex h-full w-full flex-col divide-y divide-brand-bg3">
@@ -8,18 +24,18 @@ export default function ChampionAbility() {
         <div className="flex h-full gap-4 pt-3">
           <Image
             className="w-50 h-fit border border-white"
-            src={`https://raw.communitydragon.org/latest/game/assets/characters/tft9_aatrox/hud/icons2d/tft9_aatrox_r.tft_set9.png`}
+            src={`https://raw.communitydragon.org/latest/game/${abilityIcon.toLowerCase()}`}
             alt={`Aatrox`}
             height="50"
             width="50"
           />
           <div className="flex flex-col justify-between gap-2">
             <div className="flex flex-col gap-2">
-              <p className="text-sm font-semibold lg:text-lg">World Ender</p>
-              <p className="text-xs text-white/50 lg:text-sm lg:pr-5">
-                Aatrox transforms for 12 seconds, gaining Omnivamp and
-                converting all bonus Attack Speed to Attack Damage. While
-                transformed, attacks deal physical damage.
+              <p className="text-sm font-semibold lg:text-lg">
+                {selectedChampion.ability.name}
+              </p>
+              <p className="text-xs text-white/50 lg:pr-5 lg:text-sm">
+                {cleanDesc}
               </p>
             </div>
             <p className="text-sm text-brand-main">
