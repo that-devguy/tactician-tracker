a120bdbf4e10959ffdf81b31e396ab5b764ba1b0
that_devguy <zkmutch@gmail.com>
2023-10-17 18:55:40 -0400

    added select from shadcn, updated the cost filter on champions database

--- src/app/champions/page.js
+++ src/app/champions/page.js
@@ -4,28 +4,93 @@ import Image from "next/image";
 import Link from "next/link";
 import getUnitData from "@/app/libs/getUnitData";
 import getTraitsData from "@/app/libs/getTraitsData";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/app/components/ui/select";
 
 function CostFilter({ selectedCost, onCostChange }) {
   return (
-    <div className="flex w-full px-1 gap-4 h-fit">
-      <label className="flex gap-1 items-center">
-        <Image
-          src="/coin.svg"
-          className="select-none"
-          alt="Cost Icon"
-          height="18"
-          width="18"
-        />
-        <p className="text-sm font-semibold">Cost</p>
-      </label>
-      <select value={selectedCost} onChange={onCostChange} className="bg-brand-bg3 rounded-md w-full">
-        <option value="all">All</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
+    <div className="flex h-fit w-full gap-4 px-1">
+      <Select value={selectedCost} onValueChange={onCostChange}>
+        <SelectTrigger>
+          <SelectValue placeholder="Cost" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="all">
+            <Image
+              src="/coin.svg"
+              className="select-none"
+              alt="Cost Icon"
+              height="18"
+              width="18"
+            />
+          </SelectItem>
+          <SelectItem value="1">
+            <div className="flex gap-2">
+              <Image
+                src="/coin.svg"
+                className="select-none"
+                alt="Cost Icon"
+                height="18"
+                width="18"
+              />
+              1
+            </div>
+          </SelectItem>
+          <SelectItem value="2">
+            <div className="flex gap-2">
+              <Image
+                src="/coin.svg"
+                className="select-none"
+                alt="Cost Icon"
+                height="18"
+                width="18"
+              />
+              2
+            </div>
+          </SelectItem>
+          <SelectItem value="3">
+            <div className="flex gap-2">
+              <Image
+                src="/coin.svg"
+                className="select-none"
+                alt="Cost Icon"
+                height="18"
+                width="18"
+              />
+              3
+            </div>
+          </SelectItem>
+          <SelectItem value="4">
+            <div className="flex gap-2">
+              <Image
+                src="/coin.svg"
+                className="select-none"
+                alt="Cost Icon"
+                height="18"
+                width="18"
+              />
+              4
+            </div>
+          </SelectItem>
+          <SelectItem value="5">
+            <div className="flex gap-2">
+              <Image
+                src="/coin.svg"
+                className="select-none"
+                alt="Cost Icon"
+                height="18"
+                width="18"
+              />
+              5
+            </div>
+          </SelectItem>
+        </SelectContent>
+      </Select>
     </div>
   );
 }
@@ -58,12 +123,13 @@ export default function Champions() {
 
   const units = filteredUnits.sort((a, b) => a.name.localeCompare(b.name));
 
-  const handleCostChange = (e) => {
-    setSelectedCost(e.target.value);
+  const handleCostChange = (value) => {
+    setSelectedCost(value);
+    console.log(selectedCost);
   };
 
   return (
-    <section className="px-2 pt-8 md:px-6 lg:px-6">
+    <section className="select-none px-2 pt-8 md:px-6 lg:px-6">
       <div className="mx-auto mb-2 flex max-h-28 max-w-7xl justify-between rounded-lg bg-gradient-to-tr from-[#ff571d] to-[#F8A634]">
         <div className="flex flex-col justify-center px-4 py-2 md:p-4">
           <h3 className="text-xl font-black sm:text-2xl md:text-3xl">
